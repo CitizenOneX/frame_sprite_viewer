208ef2d8980a653e9132ad14e1ffc51d1c79afd4
CitizenOneX <17427385+CitizenOneX@users.noreply.github.com>
2024-09-18 00:03:38 +1000

    first cut of sprite viewer - works for appropriately-sized 4-bit images in the default palette

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,110 @@
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:logging/logging.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+import 'package:simple_frame_app/simple_frame_app.dart';
+import 'package:simple_frame_app/tx/code.dart';
+import 'package:simple_frame_app/tx/sprite.dart';
 
-class MainApp extends StatelessWidget {
+
+void main() => runApp(const MainApp());
+
+final _log = Logger("MainApp");
+
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => MainAppState();
+}
+
+/// SimpleFrameAppState mixin helps to manage the lifecycle of the Frame connection outside of this file
+class MainAppState extends State<MainApp> with SimpleFrameAppState {
+
+  // show the loaded image in the Flutter UI also
+  Image? _image;
+
+  MainAppState() {
+    Logger.root.level = Level.INFO;
+    Logger.root.onRecord.listen((record) {
+      debugPrint('${record.level.name}: [${record.loggerName}] ${record.time}: ${record.message}');
+    });
+  }
+
+  @override
+  Future<void> run() async {
+    currentState = ApplicationState.running;
+    if (mounted) setState(() {});
+
+    try {
+      // Open the file picker
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['png'],
+      );
+
+      if (result != null) {
+        File file = File(result.files.single.path!);
+
+        // Read the file content
+        Uint8List pngBytes = await file.readAsBytes();
+
+        // Update the UI
+        setState(() {
+          _image = Image.memory(pngBytes);
+        });
+
+        // and send initial text to Frame
+        await frame!.sendMessage(TxSprite.fromPngBytes(msgCode: 0x20, pngBytes: pngBytes));
+      }
+      else {
+        currentState = ApplicationState.ready;
+        if (mounted) setState(() {});
+      }
+    } catch (e) {
+      _log.fine('Error executing application logic: $e');
+      currentState = ApplicationState.ready;
+      if (mounted) setState(() {});
+    }
+  }
+
+  @override
+  Future<void> cancel() async {
+    // remove the displayed image
+    await frame!.sendMessage(TxCode(msgCode: 0x10, value: 1));
+    _image = null;
+
+    currentState = ApplicationState.ready;
+    if (mounted) setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Frame Sprite Viewer',
+      theme: ThemeData.dark(),
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: const Text('Frame Sprite Viewer'),
+          actions: [getBatteryWidget()]
         ),
-      ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Spacer(),
+              if (_image != null) _image!,
+              const Spacer(),
+            ],
+          ),
+        ),
+        floatingActionButton: getFloatingActionButtonWidget(const Icon(Icons.file_open), const Icon(Icons.close)),
+        persistentFooterButtons: getFooterButtonsWidget(),
+      )
     );
   }
 }
